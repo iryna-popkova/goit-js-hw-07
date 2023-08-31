357dda9cacf487e1b4b2a8873a5ab4fab6ef6c5a
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-08-31 12:20:53 +0300

    begin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+
+const markup = createMarkup(galleryItems)
+
+function createMarkup(array) {
+    return array.map((preview, original, description) => {
+        return `<li> <img src = "${preview}" alt = "${description}"/>
+        </li>`
+    }).join('');
+}
