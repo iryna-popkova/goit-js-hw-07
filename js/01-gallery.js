c01012e88202a1b14b60bdd10453c965ad176da7
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-02 10:32:24 +0300

    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const container = document.querySelector(".gallery");
 
 const markup = createMarkup(galleryItems)
 
+container.insertAdjacentHTML('beforeend', markup);
+
+container.addEventListener('click', handleImageClick)
+
 function createMarkup(array) {
-    return array.map((preview, original, description) => {
-        return `<li> <img src = "${preview}" alt = "${description}"/>
-        </li>`
+    return array.map(({ preview, original, description }) => {
+        return  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
     }).join('');
 }
+
+function handleImageClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+        return;
+    }
+    
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" alt = "${event.target.alt}">
+`)
+
+    instance.show();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+
+const markup = createMarkup(galleryItems)
+
+container.insertAdjacentHTML('beforeend', markup);
+
+container.addEventListener('click', handleImageClick)
+
+function createMarkup(array) {
+    return array.map(({ preview, original, description }) => {
+        return  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', { /* options */ });
